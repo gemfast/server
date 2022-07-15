31d9563dc034d695f4812060eaa73bb3db44b3c9
Gregory Schofield <greg.c.schofield@gmail.com>
2022-07-15 15:33:02 -0400

    Add some endpoints to serve indexes
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	// "fmt"
+	// "net/http"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/gscho/gemfast/internal/indexer"
 )
@@ -13,6 +15,12 @@ type Gem struct {
 func main() {
 	i := indexer.New("/var/gemfast")
 	i.GenerateIndex()
+	r := gin.Default()
+	r.HEAD("/", func(c *gin.Context) {})
+	r.StaticFile("/specs.4.8.gz", "/var/gemfast/specs.4.8.gz")
+	r.StaticFile("/latest_specs.4.8.gz", "/var/gemfast/latest_specs.4.8.gz")
+	r.StaticFile("/prerelease_specs.4.8.gz", "/var/gemfast/prerelease_specs.4.8.gz")
+	r.StaticFile("/quick/Marshal.4.8/mixlib-install-3.0.0.gemspec.rz", "/var/gemfast/quick/Marshal.4.8/mixlib-install-3.0.0.gemspec.rz")
+	r.Run()
 	// i.UpdateIndex()
-	// marshal.Marshal()
 }
