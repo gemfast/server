8c166ae4bea5bbbb40981886a3051383a6c05737
Gregory Schofield <greg.c.schofield@gmail.com>
2022-08-25 21:01:13 -0400

    Server startup messages
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gscho/gemfast/internal/api"
 	"github.com/gscho/gemfast/internal/db"
 	"github.com/gscho/gemfast/internal/indexer"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
 func init() {
 	viper.SetEnvPrefix("GEMFAST")
 	viper.BindEnv("DIR")
+	viper.BindEnv("GEM_DIR")
 	viper.SetDefault("dir", "/var/gemfast")
+	viper.SetDefault("gem_dir", fmt.Sprintf("%s/gems", viper.Get("dir")))
 	viper.AutomaticEnv()
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
@@ -22,11 +27,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	log.Info().Msg("successfully connected to database")
 	defer db.BoltDB.Close()
 	err = indexer.InitIndexer()
 	if err != nil {
 		panic(err)
 	}
+	log.Info().Msg("indexer initialized")
+	log.Info().Msg("gemfast server ready")
 	err = api.Run()
 	if err != nil {
 		panic(err)
