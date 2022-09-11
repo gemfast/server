b833d87a5fa55e749671d44fac8e47afd85194dd
Gregory Schofield <greg.c.schofield@gmail.com>
2022-09-11 13:50:20 -0400

    Moved admin user into router
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- cmd/gemfast/main.go
+++ cmd/gemfast/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/gscho/gemfast/internal/api"
 	"github.com/gscho/gemfast/internal/db"
 	"github.com/gscho/gemfast/internal/indexer"
-	"github.com/gscho/gemfast/internal/models"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
@@ -25,28 +23,17 @@ func init() {
 }
 
 func main() {
+	log.Info().Msg("starting services")
 	err := db.Connect()
 	if err != nil {
 		panic(err)
 	}
 	defer db.BoltDB.Close()
-	log.Info().Msg("successfully connected to database")
-	err = models.CreateAdminUserIfNotExists()
-	if err != nil {
-		panic(err)
-	}
-	if strings.ToLower(viper.Get("auth").(string)) == "local" {
-		err = models.CreateLocalUsers()
-		if err != nil {
-			panic(err)
-		}
-	}
 	err = indexer.InitIndexer()
 	if err != nil {
 		panic(err)
 	}
-	log.Info().Msg("indexer initialized")
-	log.Info().Msg("gemfast server ready")
+	// indexer.Get().GenerateIndex()
 	err = api.Run()
 	if err != nil {
 		panic(err)
