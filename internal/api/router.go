d83784bd12236c931f65831cdae9b09056021692
Gregory Schofield <greg.c.schofield@gmail.com>
2022-09-11 13:49:45 -0400

    Move auth config into router
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,19 +2,42 @@ package api
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gscho/gemfast/internal/models"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
 func Run() error {
 	r := gin.Default()
+	configureAuth(r)
 	addRoutes(r)
+	log.Info().Msg("gemfast server ready")
 	return r.Run()
 }
 
-func addRoutes(r *gin.Engine) {
+func configureAuth(r *gin.Engine) {
+	authMode := viper.Get("auth").(string)
+	log.Info().Str("auth", authMode).Msg("configuring auth strategy")
+	switch strings.ToLower(authMode) {
+	case "local":
+		configureLocalAuth(r)
+	case "none":
+		configureNoneAuth(r)
+	}
+}
+
+func configureLocalAuth(r *gin.Engine) {
+	err := models.CreateAdminUserIfNotExists()
+	if err != nil {
+		panic(err)
+	}
+	err = models.CreateLocalUsers()
+	if err != nil {
+		panic(err)
+	}
 	authMiddleware, err := initAuthMiddleware()
 	if err != nil {
 		log.Error().Err(err).Msg("failed to initialize auth middleware")
@@ -27,6 +50,14 @@ func addRoutes(r *gin.Engine) {
 		authorized.POST("/api/v1/gems", uploadGem)
 		authorized.POST("/upload", geminaboxUploadGem)
 	}
+}
+
+func configureNoneAuth(r *gin.Engine) {
+	r.POST("/api/v1/gems", uploadGem)
+	r.POST("/upload", geminaboxUploadGem)
+}
+
+func addRoutes(r *gin.Engine) {
 	r.HEAD("/", head)
 	r.StaticFile("/specs.4.8.gz", fmt.Sprintf("%s/specs.4.8.gz", viper.Get("dir")))
 	r.StaticFile("/latest_specs.4.8.gz", fmt.Sprintf("%s/latest_specs.4.8.gz", viper.Get("dir")))
