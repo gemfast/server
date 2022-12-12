cb819917b2be93bbd61c953cb6e8a49e65268396
Gregory Schofield <greg.c.schofield@gmail.com>
2022-12-11 19:20:06 -0500

    Upload all gems from the .rvm directory
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- dev/upload_all.sh
+++ dev/upload_all.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd test/gems
-for gem in *.gem; do
-  [ -f "$gem" ] || break
-  gem push "$gem" --host http://localhost:8080 -k gemfast
-done
+# cd test/gems
+# gems="$HOME/.rvm/src/ruby-3.1.2/gems"
+# for gem in *.gem; do
+#   [ -f "$gem" ] || break
+#   gem push "$gem" --host http://localhost:8080 -k gemfast
+# done
+
+find $HOME/.rvm -name "*.gem" -type f -exec gem push {} --host http://localhost:8080 -k gemfast \;
