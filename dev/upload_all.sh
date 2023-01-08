42c8f41b11859a28aa391c71728f43c16a587535
gscho <greg.c.schofield@gmail.com>
2023-01-08 13:41:56 -0500

    Pipefail
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- dev/upload_all.sh
+++ dev/upload_all.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+set -euo pipefail
 
-# cd test/gems
-# gems="$HOME/.rvm/src/ruby-3.1.2/gems"
-# for gem in *.gem; do
-#   [ -f "$gem" ] || break
-#   gem push "$gem" --host http://localhost:8080 -k gemfast
-# done
-
-find $HOME/.rvm -name "*.gem" -type f -exec gem push {} --host http://localhost:8080 -k gemfast \;
+gems="$HOME/.rvm/src/ruby-3.1.2/gems"
+for gem in *.gem; do
+  [ -f "$gem" ] || break
+  echo "Uploading $gem"
+  gem push "$gem" --host http://localhost:8080 -k gemfast
+done
