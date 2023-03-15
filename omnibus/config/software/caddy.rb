f532cf574744fd0a6b607de83e07d8a8a167765b
Gregory Schofield <greg.c.schofield@gmail.com>
2023-03-15 18:19:14 -0400

    Adding support for amd64.
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- omnibus/config/software/caddy.rb
+++ omnibus/config/software/caddy.rb
@@ -4,9 +4,15 @@ default_version "2.6.2"
 license "BSD-3-Clause"
 skip_transitive_dependency_licensing true
 
-version("2.6.2") { source sha256: "5af0ee65a0220108b7b96322b0418abcda526d5f7fec5afaea029f1aebcca62a" }
+if arm?
+  version("2.6.2") { source sha256: "5af0ee65a0220108b7b96322b0418abcda526d5f7fec5afaea029f1aebcca62a" }
+  source url: "https://github.com/caddyserver/caddy/releases/download/v#{version}/caddy_#{version}_linux_amd64.tar.gz"
+else
+  version("2.6.2") { source sha256: "5af0ee65a0220108b7b96322b0418abcda526d5f7fec5afaea029f1aebcca62a" }
+  source url: "https://github.com/caddyserver/caddy/releases/download/v#{version}/caddy_#{version}_linux_amd64.tar.gz"
+end
 
-source url: "https://github.com/caddyserver/caddy/releases/download/v#{version}/caddy_#{version}_linux_amd64.tar.gz"
+
 
 build do
   mkdir "#{install_dir}/embedded/bin"
