73892fe347347851240b293572afd7c22c564855
Gregory Schofield <greg.c.schofield@gmail.com>
2023-03-30 14:40:12 -0400

    Test that rails can install from the cache of gems.
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- scripts/run_smoke_tests.sh
+++ scripts/run_smoke_tests.sh
@@ -5,6 +5,19 @@ set -ueo pipefail
 ruby --version
 bundle --version
 
+sudo mkdir -p /etc/gemfast
+sudo chown -R $USER: /etc/gemfast
+cat << ENV > /etc/gemfast/.env
+GEMFAST_PORT=2020
+GEMFAST_DIR=/var/gemfast
+GEMFAST_GEM_DIR=/var/gemfast/gems
+GEMFAST_DB_DIR=/var/gemfast/db
+GEMFAST_MIRROR=1
+GEMFAST_FILTER_ENABLED=false
+GEMFAST_FILTER_DEFAULT_DENY=false
+GEMFAST_AUTH=none
+ENV
+
 sudo dpkg -i gemfast*.deb
 sudo systemctl start gemfast
 sleep 2
@@ -19,5 +32,13 @@ cd ./clones
 
 pushd rails
 bundle config mirror.https://rubygems.org http://localhost
+bundle
+
+mv Gemfile Gemfile.backup
+touch Gemfile
+bundle clean --force
+mv Gemfile.backup Gemfile
+sed -i -e 's/https:\/\/rubygems.org/http:\/\/localhost\/private/g' Gemfile
+rm Gemfile.lock
 bundle
 popd
