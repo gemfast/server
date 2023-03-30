74c5232e60cfe924ff6681e052e22b5c6bcdadef
Gregory Schofield <greg.c.schofield@gmail.com>
2023-03-30 10:45:32 -0400

    Debugging.
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- scripts/run_smoke_tests.sh
+++ scripts/run_smoke_tests.sh
@@ -7,6 +7,13 @@ bundle --version
 
 sudo dpkg -i gemfast*.deb
 sudo systemctl start gemfast
+sleep 2
+sudo systemctl status gemfast
+sleep 2
+sudo systemctl status caddy
+
+journalctl -u gemfast
+journalctl -u caddy
 
 cd ./clones
 
