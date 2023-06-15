77a82175bcd9cf3c04de90ba256e89c6011a4449
Gregory Schofield <greg.c.schofield@gmail.com>
2023-06-15 10:49:45 -0400

    Run cve tests in a github actions job.
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

---
+++ scripts/run_cve_tests.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+ruby --version
+bundle --version
+
+gem update --system
+
+sudo mkdir -p /etc/gemfast
+sudo chown -R $USER: /etc/gemfast
+cat << CONFIG > /etc/gemfast/gemfast.hcl
+caddy_port = 80
+url = "http://localhost"
+license_key = "B7D865-DA12D3-11DA3D-DD81AE-9420D3-V3"
+auth "none" {}
+cve {
+    enabled = true
+    max_severity = "medium"
+}
+CONFIG
+
+sudo dpkg -i gemfast*.deb
+sudo systemctl start gemfast
+sleep 10
+sudo systemctl status gemfast
+sleep 2
+sudo systemctl status caddy
+
+journalctl -u gemfast
+journalctl -u caddy
+
+mkdir ./test-cve
+
+pushd test-cve
+cat << CONFIG > Gemfile
+source "https://rubygems.org"
+gem "activerecord", "4.2.0"
+CONFIG
+
+bundle config mirror.https://rubygems.org http://localhost
+bundle >/dev/null 2>&1 | grep "418"
+
+popd
