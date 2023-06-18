6eab219be7c19c53341986d210783e2d32859417
Gregory Schofield <greg.c.schofield@gmail.com>
2023-06-18 12:52:09 -0400

    Use sudo tee.
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- scripts/run_filter_tests.sh
+++ scripts/run_filter_tests.sh
@@ -45,7 +45,8 @@ bundle
 
 popd
 
-cat << CONFIG > /etc/gemfast/gemfast.hcl
+sudo rm -rf /etc/gemfast/gemfast.hcl
+sudo tee /etc/gemfast/gemfast.hcl > /dev/null <<'CONFIG'
 caddy_port = 80
 url = "http://localhost"
 license_key = "B7D865-DA12D3-11DA3D-DD81AE-9420D3-V3"
@@ -56,7 +57,7 @@ filter {
     regex = ["active.*"]
 }
 CONFIG
-
+sudo chown -R $USER: /etc/gemfast
 sudo systemctl restart gemfast
 sleep 5
 sudo systemctl status gemfast
@@ -71,6 +72,7 @@ source "https://rubygems.org"
 gem "activesupport", "~> 7.0", ">= 7.0.5"
 CONFIG
 
+bundle config mirror.https://rubygems.org http://localhost
 if [[ $(bundle 2>&1 | grep "405") ]]; then
     echo "Gemfast is working"
 else
